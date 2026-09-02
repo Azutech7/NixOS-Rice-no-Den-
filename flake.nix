caf79fe854e75301e51c37ae5b564cfa7b3229a9
Mason Heuberger <110648371+Azutech7@users.noreply.github.com>
2026-09-02 10:52:21 -0700

    ye ah

--- flake.nix
+++ flake.nix
@@ -1,41 +1,41 @@
 {
-  description = "Azutech NixOS Rice";
+    description = "Azutech NixOS Rice";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
-
-
-
-    nixosConfigurations.<HOST_NAME> = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      
-      specialArgs = { inherit inputs; }; 
-
-      modules = [
-        ./hosts/<HOST_NAME>/config.nix
-
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-
-          home-manager.extraSpecialArgs = { inherit inputs; };
-          
-          home-manager.users = {
-            <USER_NAME> = import ./users/<USER_NAME>/config.nix;
-          };
-
-        }
-      ];
+        home-manager.url = "github:nix-community/home-manager";
+        home-manager.inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    outputs = { self, nixpkgs, home-manager, ... }@inputs: {
 
 
-  };
+
+        nixosConfigurations.<HOST_NAME> = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+
+            specialArgs = { inherit inputs; }; 
+
+            modules = [
+                ./hosts/<HOST_NAME>/config.nix
+
+                home-manager.nixosModules.home-manager
+                {
+                    home-manager.useGlobalPkgs = true;
+                    home-manager.useUserPackages = true;
+
+                    home-manager.extraSpecialArgs = { inherit inputs; };
+
+                    home-manager.users = {
+                        <USER_NAME> = import ./users/<USER_NAME>/config.nix;
+                    };
+
+                }
+            ];
+        };
+
+
+
+    };
 }
